0178ab1dde816ba29e752b3d61457e701c429ba6
Felicia Sabatini <feliciasabatinigunawan2005@gmail.com>
2024-10-14 20:19:17 +0700

    database yang bener nah

--- todolist.sql
+++ todolist.sql
@@ -16,6 +16,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `list`
+--
+
+DROP TABLE IF EXISTS `list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `list` (
+  `listid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  `category` varchar(20) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `deadline` datetime DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  `done` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`listid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `list`
+--
+
+LOCK TABLES `list` WRITE;
+/*!40000 ALTER TABLE `list` DISABLE KEYS */;
+INSERT INTO `list` VALUES (1,'Project',NULL,'Work',3,'2024-10-12 22:29:27','Done Late',NULL),(6,'miawmiawmiaw','aaa','Personal',1,NULL,'Not Done',NULL),(7,'Makan','Makan yang sehat biar kuat','Personal',3,NULL,'Done',NULL);
+/*!40000 ALTER TABLE `list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -30,7 +60,7 @@ CREATE TABLE `user` (
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`userid`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +69,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'miaw','miaw@gmail.com','2024-01-01','$2y$10$It.j81wQmyN40filk26FY.HJMbbU4qNvlDGx0Qe0kCRi6u1i/GWA6');
+INSERT INTO `user` VALUES (1,'miaw','miaw@gmail.com','2024-01-01','$2y$10$HCTd/qLQCCHI0sa6fSQdH.DHJRA9S5PMeEmHKYz7y0Fi7zZI9dAMa'),(2,'males','males@gmail.co.id','2024-10-01','$2y$10$de5ueYnplEppPlwt5ETS/eark09Jdo3X2K2JylL9TxDaVJjtpBV4G'),(3,'Felicia625','feliciasabatinigunawan2005@gmail.com','2005-02-06','$2y$10$6o2oZ9xOvW2Ex.9D/iP./eSU.GVFjqTwPkZh8hEqugJX/CHTMI3tK');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-12 22:56:14
+-- Dump completed on 2024-10-14 20:16:44
