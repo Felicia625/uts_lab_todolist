4fd8a1963f8833e5a40381be2d6b2affa69836ff
Felicia Sabatini <feliciasabatinigunawan2005@gmail.com>
2024-10-16 11:22:15 +0700

    navbar fix

--- todolist/frontend/edit_task.php
+++ todolist/frontend/edit_task.php
@@ -88,5 +88,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
             </div> 
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
